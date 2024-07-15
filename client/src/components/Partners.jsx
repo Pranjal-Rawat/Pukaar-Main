77ece5e546ddb8e108a92f571131e45c26e7df7c
Pranjal-Rawat <145280526+Pranjal-Rawat@users.noreply.github.com>
2024-07-15 22:40:10 +0530

    Update Partners.jsx

--- client/src/components/Partners.jsx
+++ client/src/components/Partners.jsx
@@ -17,7 +17,7 @@ export default function Partners() {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
                                     </svg>
-                                    <div className="ml-4 text-xl">Project Purkul</div>
+                                    <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.projectpurkul.com/">Project Purkul</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Project Purkul is a cloth-based handicrafts collective driven by intricacies of technique. We use patchwork, applique and quilting methodologies to create pieces that decorate your home, add functionality and color to your kitchen, and bring a balanced blend of comfort and beauty to your personal accessories.</p>
                             </div>
@@ -27,7 +27,7 @@ export default function Partners() {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
                                     </svg>
-                                    <div className="ml-4 text-xl">Gyanoday</div>
+                                    <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://gyanoday.org.in/">Gyanoday</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Gyanoday works towards the upliftment of the neglected and marginalized sections of the society. Since 2001, we have been striving to ensure that individuals and children from these communities are given the opportunity to grow into constructive and self-sufficient citizens of tomorrow by creating a nurturing environment for them today.</p>
                             </div>
@@ -37,7 +37,7 @@ export default function Partners() {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
                                     </svg>
-                                    <div className="ml-4 text-xl">Himaagman Foundation</div>
+                                    <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.facebook.com/khadimissionwardha/">Himaagman Foundation</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Himaagman Foundation, we are reaching out to you with an opportunity to make a significant impact on the lives of 150 children from underprivileged communities through our initiative, "Empowering Futures." This project aims to promote academic education among these children, providing them with essential teaching and learning materials, school bags, and uniforms.</p>
                             </div>
@@ -49,7 +49,7 @@ export default function Partners() {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
                                     </svg>
-                                    <div className="ml-4 text-xl">Ucost</div>
+                                    <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://ucost.uk.gov.in/">UCOST</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Uttarakhand State Council for Science and Technology is an autonomous body of the Government of Uttarakhand, Department of Information & Science Technology. UCOST started its activities from last quarter of year 2005, However it was registered under the Registration of Societies Act, 1860 in November 2002.</p>
                             </div>
@@ -59,7 +59,7 @@ export default function Partners() {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
                                     </svg>
-                                    <div className="ml-4 text-xl">Giri Foundation</div>
+                                    <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.girifoundation.org/">Giri Foundation</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Giri Foundation is a global Non-Profit Organization that aims to uplift the lives of 1 million Himalayan women in the coming years. To achieve this, the foundation will harness the passion and commitment of these women by providing them skill training, focusing on business and financial literacy, management skills, and confidence building.</p>
                             </div>
@@ -69,7 +69,7 @@ export default function Partners() {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
                                     </svg>
-                                    <div className="ml-4 text-xl">Project Aaina</div>
+                                    <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.projectaaina.in/">Project Aina</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Project Aaina is an organization that started with the objective of self-reflection. The purpose of this initiative is to stream into the inner consciousness of an individual through an organic artistic process. We curate a diverse range of events, spanning from immersive mindfulness retreats and community welfare activities to engaging one-day experiences that facilitate a connection.</p>
                             </div>
@@ -79,7 +79,7 @@ export default function Partners() {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
                                     </svg>
-                                    <div className="ml-4 text-xl">Hindska Trust</div>
+                                    <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.hindskatrust.org/">Hindska Trust</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Hindska Trust, we believe that education is a fundamental right of every individual, regardless of their background, economic ability, socioeconomic status, or geographic location. Hindska is driven by the belief that education is the key to empowering individuals and communities, fostering economic growth, and promoting social progress.</p>
                             </div>
@@ -89,7 +89,7 @@ export default function Partners() {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
                                     </svg>
-                                    <div className="ml-4 text-xl">Zindagi Mubarak</div>
+                                    <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.instagram.com/zindagi.mubarak/">Zindagi Mubarak</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">A #festival about celebrating life 🌼🌿A venture by @ishqestantv and @meghna_chatterji</p>
                             </div>
