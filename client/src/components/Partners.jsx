0899d9501d94f07c69b66842b05106be16faa818
Pranjal-Rawat <145280526+Pranjal-Rawat@users.noreply.github.com>
2024-07-18 23:38:07 +0530

    Update Partners.jsx

--- client/src/components/Partners.jsx
+++ client/src/components/Partners.jsx
@@ -16,11 +16,9 @@ export default function Partners() {
                         <div className="flex flex-wrap my-12">
                             <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-4">
                                 <div className="flex items-center mb-6">
-{/*                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
-                                    </svg> */}
-
-                                    <img src="https://static.wixstatic.com/media/9dabf8_101aeaca1eac4074bac04acd13066ff4~mv2.png/v1/crop/x_0,y_46,w_1424,h_1330/fill/w_101,h_94,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/final%2520logo%2520PNG-01_edited.png"></img>
+                                    </svg>
                                     <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.projectpurkul.com/">Project Purkul</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Project Purkul is a cloth-based handicrafts collective driven by intricacies of technique. We use patchwork, applique and quilting methodologies to create pieces that decorate your home, add functionality and color to your kitchen, and bring a balanced blend of comfort and beauty to your personal accessories.</p>
@@ -28,11 +26,9 @@ export default function Partners() {
 
                             <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-4">
                                 <div className="flex items-center mb-6">
-{/*                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
-                                    </svg> */}
-
-                                    <img src="https://gyanoday.org.in/wp-content/uploads/2020/04/Logo-02-02-1024x298.webp"></img>
+                                    </svg>
                                     <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://gyanoday.org.in/">Gyanoday</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Gyanoday works towards the upliftment of the neglected and marginalized sections of the society. Since 2001, we have been striving to ensure that individuals and children from these communities are given the opportunity to grow into constructive and self-sufficient citizens of tomorrow by creating a nurturing environment for them today.</p>
@@ -52,11 +48,9 @@ export default function Partners() {
                         <div className="flex flex-wrap">
                             <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-4">
                                 <div className="flex items-center mb-6">
-{/*                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
-                                    </svg> */}
-
-                                    <img src="https://ucost.uk.gov.in/sites/default/files/logo-final.png"></img>
+                                    </svg>
                                     <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://ucost.uk.gov.in/">UCOST</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Uttarakhand State Council for Science and Technology is an autonomous body of the Government of Uttarakhand, Department of Information & Science Technology. UCOST started its activities from last quarter of year 2005, However it was registered under the Registration of Societies Act, 1860 in November 2002.</p>
@@ -64,11 +58,9 @@ export default function Partners() {
 
                             <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-4">
                                 <div className="flex items-center mb-6">
-{/*                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
-                                    </svg> */}
-
-                                    <img src="https://www.girifoundation.org/image/logo.PNG"></img>
+                                    </svg>
                                     <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.girifoundation.org/">Giri Foundation</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Giri Foundation is a global Non-Profit Organization that aims to uplift the lives of 1 million Himalayan women in the coming years. To achieve this, the foundation will harness the passion and commitment of these women by providing them skill training, focusing on business and financial literacy, management skills, and confidence building.</p>
@@ -76,12 +68,9 @@ export default function Partners() {
 
                             <div className="w-full md:w-1/2 lg:w-1/3 lg:border-r-0 p-4">
                                 <div className="flex items-center mb-6">
-{/*                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
-                                    </svg> */}
-
-                                    <img src="https://lh6.googleusercontent.com/GlPg8gqlpaC3qfYD0CBRmMCuUxiGGsYIuinWYMQA2vYkxfX-xdrj6gkYlOlgnjJ_lK8oDk6Pz2MfJMv_yLn6wE8=w16383"></img>
-                                    
+                                    </svg>
                                     <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.projectaaina.in/">Project Aina</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Project Aaina is an organization that started with the objective of self-reflection. The purpose of this initiative is to stream into the inner consciousness of an individual through an organic artistic process. We curate a diverse range of events, spanning from immersive mindfulness retreats and community welfare activities to engaging one-day experiences that facilitate a connection.</p>
@@ -89,13 +78,9 @@ export default function Partners() {
 
                             <div className="w-full md:w-1/2 lg:w-1/3 lg:border-r p-4">
                                 <div className="flex items-center mb-6">
-{/*                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
-                                    </svg> */}
-
-                                    <img src="https://static.wixstatic.com/media/35758e_cdf8a135e4a841d1975128a25622b1ca~mv2.png/v1/fill/w_148,h_48,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/white%20logo.png"></img>
-
-                                    
+                                    </svg>
                                     <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.hindskatrust.org/">Hindska Trust</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">Hindska Trust, we believe that education is a fundamental right of every individual, regardless of their background, economic ability, socioeconomic status, or geographic location. Hindska is driven by the belief that education is the key to empowering individuals and communities, fostering economic growth, and promoting social progress.</p>
@@ -103,11 +88,9 @@ export default function Partners() {
 
                             <div className="w-full md:w-1/2 lg:w-1/3 lg:border-r-0 p-4">
                                 <div className="flex items-center mb-6">
-{/*                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="currentColor" className="h-6 w-6 text-gray-500">
                                         <path d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z"></path>
-                                    </svg> */}
-
-                                    <img src="">Not found</img>
+                                    </svg>
                                     <div className="ml-4 text-xl"><a class="ml-4 text-xl" href="https://www.instagram.com/zindagi.mubarak/">Zindagi Mubarak</a></div>
                                 </div>
                                 <p className="leading-loose text-gray-500">A #festival about celebrating life 🌼🌿A venture by @ishqestantv and @meghna_chatterji</p>
