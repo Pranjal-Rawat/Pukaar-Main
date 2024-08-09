c12510498fe33df17a2b804387727d1e80e812c3
Pranjal-Rawat <145280526+Pranjal-Rawat@users.noreply.github.com>
2024-08-09 10:57:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,15 @@ app.get('/' , ( req,res) =>{
     res.send("Server Live Pukaar") ;
 })
 
+app.post('/contact',(req,res)=>{
+    res.send("Done");
+});
+
 
 app.listen(3000, () => {
     console.log("Server Live Pukaar")
 })
+
+const URI = "mongodb+srv://pukaarsanstha:I60868V899LQREow@cluster0.pv61dno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(URI).then().catch((err)=>{});
